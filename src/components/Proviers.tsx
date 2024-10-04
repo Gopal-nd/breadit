18566d6abffa97414e33c15488569bb90b3242de
Gopal <gopalhelpyou@gmail.com>
2024-10-04 15:34:38 +0530

    ceatedfeed

--- src/components/Proviers.tsx
+++ src/components/Proviers.tsx
@@ -1,12 +1,16 @@
 'use client';
 
 import { QueryClient,QueryClientProvider } from "@tanstack/react-query";
+import { SessionProvider } from "next-auth/react";
+import React from "react";
+
 export default function Providers({ children }: { children: React.ReactNode }) {
 
     const queryClient = new QueryClient();
-    return <>
-    <QueryClientProvider client={queryClient}>
+    return <QueryClientProvider client={queryClient}>
+    <SessionProvider>
+
     {children}
-    </QueryClientProvider>
-    </>;
+    </SessionProvider>
+    </QueryClientProvider>;
 }
